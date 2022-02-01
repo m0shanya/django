333004403fdeefcca8f221b4187208a73eece35a
timofey <funnytimka@gmail.com>
2022-02-02 02:02:58 +0300

    add cw 17(+)

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,12 +1,22 @@
 import logging
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from blog.forms import RegisterForm
+
+from django.contrib.auth.models import User
 
 logger = logging.getLogger(__name__)
 
 
 def register(request):
-    form = RegisterForm()
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            # Process validated data
+            logger.info(form.cleaned_data)
+            user = User(email=form.cleaned_data['email'])
+            return redirect("/")
+    else:
+        form = RegisterForm()
     return render(request, "register.html", {"form": form})
